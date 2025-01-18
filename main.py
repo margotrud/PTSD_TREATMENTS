7992784becff08b2743b46b66b694ac78e861908
Margot <margot.rudnianski@gmail.com>
2025-01-18 16:39:10 +0100

    Initial commit giving around 250 results

--- main.py
+++ main.py
@@ -39,25 +39,11 @@ if __name__ == "__main__":
         r"(life (changing|saving)|recommend)",
         r"I[' ]?(felt|have experienced|am finding|worked|works|tried|have taken it)",
         rf"during the {therapy}"
-
-        
-        #
-        # r"I[' ]?had a few sessions", r"I did a few sessions", r"I[' ]?(treated|did|tried)",rf"I found that (it|{therapy})",
-        # r"I attended sessions", r"The method used was", rf"The process of {therapy}",
-        # rf"What happened during {therapy}",
-        # r"How the therapist guided", rf"The steps of {therapy}", r"I experience", r"I went", r"I process",
-        # r"experience for me", r"for me", r"it (helps|helped|worked)", r"it didn't (helped|worked)",
-        # rf"{therapy} played a role", r"(saved|changed)", r"traumatized me", r"retraumatized me", rf"during the {therapy}",
-        # r"life (changing|saving)", r"I went from", r"made a difference", r"(helped|help)", r"it (made|did)", r"I felt",
-        # rf"did {therapy}", r"the process", r"helpful", rf"{therapy} changed", r"recommend", r"I have taking it",
-        # r"I didin't have", r"I'm finding it", r"I have experienced",r"I've had", r"worked", r"works", r"does not work",
-        # r"It did nothing", r"I tried", r"I'm doing it"
-
     ]
 
     exclusion_keywords = [
         rf"I[' ]?am a therapist", rf"{therapy} therapist here", r"coming from a board[-]?certified", r"my clients",
-        r"I[' ]?m ineligible", r"I did (DBT|CBT|other therapies)", r"(in case|incase) this does not work", r"have you found",
+        r"I[' ]?m ineligible", r"I did (DBT|CBT|other therapies)", r"have you found",
         r"someone who did it", r"not a problem for me", r"(tapping|research)", r"^How.*\?$",
     ]
 
